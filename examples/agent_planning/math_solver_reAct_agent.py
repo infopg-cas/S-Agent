8db246dabab75c3f8f206c19d131e2bc4ebefb78
hectorrrrr <zhilinhe@usc.edu>
2024-06-02 16:24:00 -0700

    adjust

--- examples/agent_planning/math_solver_reAct_agent.py
+++ examples/agent_planning/math_solver_reAct_agent.py
@@ -25,11 +25,12 @@ if __name__ == "__main__":
     # "1. You don't generate the Observation step, user will generate it.\n"
     # "4. If there is no tools for you to choose, give the answer by yourself.\n" \
 
+
     prompt = "You are a math teach agent, you job is to solve the problem for the user. " \
              "You have a tool library\n:" \
              "1. Name: 'calculate_square', which calculate the square root.\n" \
              "2. Name: 'add_two', which adds two numbers.\n" \
-             "I want to you generate the answer step by step, you will move one step each time. You have 4 steps options: Think, Action, Observation, Finish:\n" \
+             "I want to you generate the answer step by step, you will move one step each time. You have 4 Action options: Think, Action, Observation, Finish:\n" \
              "For action step, you will tell me the action tool name that you want to call, and I will call it and give you the result in Observation." \
              "When you get the answer, give the result in Finish step.\n" \
              "Restrictions\n: " \
@@ -53,6 +54,6 @@ if __name__ == "__main__":
         },
         template=prompt
     )
-    obs, r, j = agent.run_agent("What's the difference of square root of 3 and the square root of 259?")
-    # obs, r, j = agent.run_agent("What's the history of math?")
+    # cobs, r, j = agent.run_agent("What's the difference of square root of 3 and the square root of 259?")
+    obs, r, j = agent.run_agent("What's the weather today?")
     pprint.pprint(j)
